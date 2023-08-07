b0dd6aef044836dd53b16fe48a7a699ce267544a
Alvin <alvinreggaelaroya@gmail.com>
2023-08-07 22:39:31 +0800

    initial commit

--- src/lib/components/sections/AboutMe.svelte
+++ src/lib/components/sections/AboutMe.svelte
@@ -1,19 +1,23 @@
+<script>
+  import AboutMeImages from "./AboutMeImages.svelte";
+</script>
+
 <div class="mt-[2rem] md:mt-[15rem] md:my-28">
   <div class="text-center">
     <h1 class="text-4xl">About Me</h1>
     <p>My Introduction</p>
   </div>
 </div>
-<div class="grid grid-cols-1 md:grid-cols-2 mt-7">
-  <div class="order-2 md:order-1 flex md:px-24 justify-center">
-    <img src="/johny.jpg" alt="" class="w-[100%] lg:w-full" />
+<div class="grid grid-cols-1 md:grid-cols-2 mt-7 md:px-32">
+  <div class="order-2 md:order-1 flex md:px-24 justify-center -mt-8 md:mt-0">
+    <AboutMeImages />
   </div>
-  <div class="order-1 md:order-2">
-    <p class="text-gray-600 text-sm font-thin md:pr-[38rem]">
+  <div class="order-1 md:order-2 grid h-[420px] content-center">
+    <p class="text-gray-600 text-sm font-thin md:pr-[29rem] text-justify">
       Web developer with extensive knowledge and years of experience working in
       web technologies and UI/UX design delivering quality work.
     </p>
-    <div class="grid grid-cols-3 md:pr-[30rem] mt-8">
+    <div class="grid grid-cols-3 md:pr-[20rem] mt-8">
       <div class="text-center">
         <h1 class="text-2xl">3+</h1>
         <p class="font-thin text-gray-600 text-sm">Years experience</p>
@@ -29,7 +33,7 @@
     </div>
     <button
       type="button"
-      class="w-full md:w-auto mt-9 text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-normal rounded-lg text-lg px-5 py-2.5 text-center mr-2 mb-2"
+      class="w-full md:w-48 mt-9 text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-normal rounded-lg text-lg px-5 py-4 text-center mr-2 mb-2"
       >Download CV</button
     >
   </div>
---
+++ src/lib/components/sections/AboutMeImages.svelte
@@ -0,0 +1,14 @@
+<div class="grid grid-cols-2 gap-2">
+  <div
+    class="bg-[url('/workshop4.jpg')] h-40 md:h-52 w-40 md:w-52 bg-cover bg-no-repeat rounded-lg"
+  />
+  <div
+    class="bg-[url('/workshop3.jpg')] h-40 md:h-52 w-40 md:w-52 bg-cover bg-no-repeat rounded-lg"
+  />
+  <div
+    class="bg-[url('/workshop2.jpg')] h-40 md:h-52 w-40 md:w-52 bg-cover bg-no-repeat rounded-lg"
+  />
+  <div
+    class="bg-[url('/workshop1.jpg')] h-40 md:h-52 w-40 md:w-52 bg-cover bg-no-repeat rounded-lg"
+  />
+</div>
