f2fea43a96e65028a73bcca0b77552513b598bdc
alvinlaroya <alvinreggaelaroya@gmail.com>
2023-08-18 00:21:48 +0800

    feat: added email sent toast

--- src/lib/components/sections/ContactMe.svelte
+++ src/lib/components/sections/ContactMe.svelte
@@ -1,14 +1,27 @@
 <script>
-    import { Label, Input, Textarea, Button } from "flowbite-svelte";
+    import {
+        Label,
+        Input,
+        Textarea,
+        Button,
+        Spinner,
+        Toast,
+    } from "flowbite-svelte";
+    import { Icon } from "flowbite-svelte-icons";
 
     import emailjs from "@emailjs/browser";
     import HeaderTitle from "./HeaderTitle.svelte";
+
+    let emailSent = false;
+    let sending = false;
 
     let from_name = "";
     let from_email = "";
     let message = "";
 
     const sendEmail = async () => {
+        sending = true;
+
         var templateParams = {
             from_name,
             from_email,
@@ -25,11 +38,23 @@
             .then(
                 function (response) {
                     console.log("SUCCESS!", response.status, response.text);
+                    sending = false;
+                    emailSent = true;
+
+                    resetForm();
                 },
                 function (error) {
                     console.log("FAILED...", error);
                 }
             );
+
+        setTimeout(() => (emailSent = false), 3000);
+    };
+
+    const resetForm = () => {
+        from_name = "";
+        from_email = "";
+        message = "";
     };
 
     let textareaprops = {
@@ -42,9 +67,27 @@
 </script>
 
 <HeaderTitle title="Contact Me" subtitle="Send Me A Message" />
-<div class="flex container mx-auto mt-16">
-    <div class="w-1/2 mx-auto">
-        <div class="mb-6">
+<div class="container grid grid-cols-1 md:grid-cols-2 content-center mt-16">
+    <div>
+        <img src="/wp5603752.jpg" alt="" />
+    </div>
+    <div>
+        {#if emailSent}
+            <Toast
+                transition="slide"
+                simple
+                contentClass="flex space-x-4 divide-x divide-gray-200 dark:divide-gray-700"
+            >
+                <Icon
+                    name="papper-plane-outline"
+                    class="w-5 h-5 text-green-500 dark:text-green-500 rotate-45"
+                />
+                <div class="pl-4 text-sm font-normal">
+                    Message sent successfully.
+                </div>
+            </Toast>
+        {/if}
+        <div class="mb-6 mt-6">
             <Label for="large-input" class="block mb-2">Name</Label>
             <Input
                 bind:value={from_name}
@@ -66,9 +109,13 @@
             <Textarea bind:value={message} {...textareaprops} />
         </div>
         <div class="flex w-full justify-end">
-            <Button size="lg" color="purple" on:click={sendEmail}
-                >Send Message</Button
-            >
+            <Button size="lg" color="purple" on:click={sendEmail}>
+                {#if sending}
+                    <Spinner class="mr-3" size="4" color="white" />Sending ...
+                {:else}
+                    <span>Send Message</span>
+                {/if}
+            </Button>
         </div>
     </div>
 </div>
